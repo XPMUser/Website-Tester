f22b575ea9e5b81e46d65c67278a78ba36d865b2
PeekoMeeko <62924215+Toonigy@users.noreply.github.com>
2025-08-02 16:15:27 -0700

    Update mod-handler.js

--- pde1500/js/mod-handler.js
+++ pde1500/js/mod-handler.js
@@ -164,7 +164,7 @@ class ModHandler {
 				if (Util.isDefined(h))
 					for (var f = h.getAll(), y = 0, v = h.getTweens(); y < f.length; y++) v[y].delay = f[y].baseDelay / g, v[y].duration = v[y].frames.length * f[y].baseDelay * f[y].baseDuration / g
 			}
-		}, window.setGameSpeed(3), setTimeout((() => {
+		}, window.setGameSpeed(20), setTimeout((() => {
 			this.info('Use "setGameSpeed(speed)" to change the game speed at anytime.')
 		}), 1e3)
 	}
@@ -501,4 +501,5 @@ function checkForMods(e, t) {
 			ModHooks.error('Error occured while applying mod "' + GameMods.available[a].id + '" to Prodigy!'), console.error(e)
 		}
 	i > 0 ? ModHooks.log("Applied (" + i + ") mods to the game") : ModHooks.log("No mods have been applied")
+
 }
